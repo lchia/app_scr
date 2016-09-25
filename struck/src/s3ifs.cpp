23bdde5e4033287657686ae9aa69a4b2d3af7e50
lchia <lichenghua2014@ia.ac.cn>
2016-09-25 03:25:28 -0400

    modified s3ifs

--- struck/src/s3ifs.cpp
+++ struck/src/s3ifs.cpp
@@ -7,14 +7,584 @@ s3ifs::s3ifs(SpMatRd X, SpMatRd D)
 	{
 		std::cout << "rbu: " << rbu << std::endl;
 	}
+	X_ = X;
+	D_ = D;
+    X_CM_ = X_;
+    n_sams_ = X_.rows();
+    n_feas_ = X_.cols();
 
-    //ssvm_sifs solver(input_fn, alpha, beta, gam, tol, max_iter, chk_fre, scr_max_iter);
+    inv_n_sams_ = 1.0 / static_cast<double>(n_sams_);
+    inv_gamma_ = 1.0 / gamma_;
+    inv_alpha_ = 1.0 / alpha_;
+	
+    one_over_XTones_ =
+        inv_n_sams_ * (X_CM_.transpose() * Eigen::VectorXd::Ones(n_sams_)).array();
+
+    beta_max_ = one_over_XTones_.abs().maxCoeff();
+    if (beta_ >= beta_max_) {
+        std::cout << "beta >= beta_max: always have naive solutions" << std::endl;
+    }
+
+    Eigen::ArrayXd temp =
+        (one_over_XTones_ > beta_).select(one_over_XTones_ - beta_, 0.0) +
+        (one_over_XTones_ < -beta_).select(one_over_XTones_ + beta_, 0.0);
+ 
+    alpha_max_ = (1.0/(1.0 - gamma_)) * (X_ * temp.matrix()).maxCoeff();
+
+    if (alpha_max_ > 0) {
+        ref_alpha_ = alpha_max_;
+        ref_dsol_ = Eigen::VectorXd::Ones(n_sams_);	//theta*
+        ref_psol_ = temp * (1.0 / ref_alpha_);		// w*
+        dif_alpha_ratio_ = 0.5 * (alpha_ - ref_alpha_) / alpha_;
+        sum_alpha_ratio_ = 0.5 * (alpha_ + ref_alpha_) / alpha_;
+    } else{
+        ref_alpha_ = 0;
+        ref_dsol_ = Eigen::VectorXd::Ones(n_sams_);
+        ref_psol_ = Eigen::VectorXd::Zero(n_feas_); //Why? ->
+        dif_alpha_ratio_ = 0.5;
+        sum_alpha_ratio_ = 0.5;
+    } 
+
+    dsol_ = Eigen::VectorXd::Ones(n_sams_);	//Dual optimum
+    Xw_comp_ = dsol_;
+    psol_ = Eigen::VectorXd::Zero(n_feas_);	//Primal optimum
+    XTdsol_ = Eigen::VectorXd::Zero(n_feas_);
+
+    Xi_norm_.resize(n_sams_);
+    for (int i = 0; i < n_sams_; ++i) {
+        Xi_norm_[i] = X_.row(i).norm();
+    }
+
+    Xj_norm_.resize(n_feas_);
+    for (int j = 0; j < n_feas_; ++j) {
+        Xj_norm_[j] = X_CM_.col(j).norm();
+    } 
+	
+    all_ins_index_.resize(n_sams_);
+    std::iota(std::begin(all_ins_index_), std::end(all_ins_index_), 0);
+    all_fea_index_.resize(n_feas_);
+    std::iota(std::begin(all_fea_index_), std::end(all_fea_index_), 0);
+
+    idx_Fc_flag_ = Eigen::VectorXd::Ones(n_feas_);
+    idx_Dc_flag_ = Eigen::VectorXd::Ones(n_sams_);
+
+    idx_Dc_.clear();
+    idx_Fc_.clear();
+    idx_Dc_.insert(std::begin(all_ins_index_), std::end(all_ins_index_));
+    idx_Fc_.insert(std::begin(all_fea_index_), std::end(all_fea_index_));
+
+    idx_nsv_L_.clear();
+    idx_nsv_R_.clear();
+    idx_F_.clear();
+
+    std::copy(std::begin(all_ins_index_), std::end(all_ins_index_),
+              std::back_inserter(idx_Dc_vec_));
+    std::copy(std::begin(all_fea_index_), std::end(all_fea_index_),
+              std::back_inserter(idx_Fc_vec_));
+
+    approx_dsol_r_L_sq_ = 0.0;
+    approx_dsol_r_R_sq_ = 0.0;
+    approx_psol_r_F_sq_ = 0.0;
+    
+    fea_scr_time_ = 0.0;
+    sam_scr_time_ = 0.0;
+    scr_time_ = 0.0;
+
+    iter_ = 0;
+    duality_gap_ = std::numeric_limits<double>::max();
+    //s3ifs solver(input_fn, alpha, beta, gam, tol, max_iter, chk_fre, scr_max_iter);
 }
 
 s3ifs::~s3ifs()
 {}
 
-void s3ifs::parse_command_line() {
+int s3ifs::get_n_sams(void) const { return n_sams_; }
+int s3ifs::get_n_feas(void) const { return n_feas_; }
+
+double s3ifs::get_primal_obj(void) const { return pobj_; }
+double s3ifs::get_dual_obj(void) const { return dobj_; }
+double s3ifs::get_duality_gap(void) const { return duality_gap_; }
+
+int s3ifs::get_n_L(void) const { return idx_nsv_L_.size(); }
+int s3ifs::get_n_R(void) const { return idx_nsv_R_.size(); }
+int s3ifs::get_n_F(void) const { return idx_F_.size(); }
+
+int s3ifs::get_iter(void) const { return iter_; }
+
+double s3ifs::get_alpha_max(void) const { return alpha_max_; }
+double s3ifs::get_beta_max(void) const { return beta_max_; }
+
+Eigen::VectorXd s3ifs::get_dual_sol(void) const { return dsol_; }
+Eigen::VectorXd s3ifs::get_primal_sol(void) const { return psol_; }
+
+void s3ifs::set_stop_tol(const double& tol) { tol_ = tol; }
+
+void s3ifs::compute_primal_obj(const bool& flag_comp_loss) 
+{
+    pobj_ = (.5 * alpha_) * psol_.squaredNorm() + (beta_) * psol_.lpNorm<1>();
+
+    loss_ = 0.0;
+    if (flag_comp_loss)
+	{
+        Xw_comp_ = 1 - (X_ * psol_).array();
+        double Xw_comp_i;
+        for (int i = 0; i < n_sams_; ++i) 
+		{
+            Xw_comp_i = Xw_comp_[i];
+            if (Xw_comp_i > gamma_)
+			{
+                loss_ += Xw_comp_i - .5 * gamma_;
+            } 
+			else if (Xw_comp_i > 0.0) 
+			{
+                loss_ += .5 * Xw_comp_i * Xw_comp_i * inv_gamma_;
+            }
+        }
+    }
+    pobj_ = pobj_ + loss_ * inv_n_sams_;
+}
+
+void s3ifs::compute_dual_obj(const bool& flag_comp_XTdsol) 
+{
+    if (flag_comp_XTdsol)
+	{
+        update_psol(true);
+    }
+    Eigen::ArrayXd temp = XTdsol_.array().abs() - beta_;
+    dobj_ =
+        (dsol_.sum() - (.5 * gamma_) * dsol_.squaredNorm()) * inv_n_sams_
+        - (0.5 * inv_alpha_) * (temp >= 0.0).select(temp, 0.0).square().sum();
+}
+
+void s3ifs::compute_duality_gap(const bool& flag_comp_loss, const bool& flag_comp_XTdsol) 
+{
+    compute_primal_obj(flag_comp_loss);
+    compute_dual_obj(flag_comp_XTdsol);
+
+    duality_gap_ = std::max(0.0, pobj_ - dobj_);
+}
+
+void s3ifs::update_psol(const bool& flag_comp_XTdsol) {
+    if (flag_comp_XTdsol) {
+        XTdsol_.setZero();
+        for (int i = 0; i < n_sams_; ++i) {
+            if (dsol_[i] > 0.0) {
+                XTdsol_ += dsol_[i] * X_.row(i);
+            }
+        }
+        XTdsol_ *= inv_n_sams_;
+    }
+
+    for (int i = 0; i < n_feas_; ++i) {
+        psol_[i] = val_sign(XTdsol_[i]) * inv_alpha_ *
+            std::max(0.0, std::abs(XTdsol_[i]) - beta_);
+    }
+}
+
+void s3ifs::train_sifs(const int& scr_option) 
+{
+    // we have closed form solution, if alpha > alpha_max(beta)
+    if (alpha_ >= alpha_max_) {
+        dsol_.setOnes();
+        Eigen::ArrayXd temp =
+            (one_over_XTones_ > beta_).select(one_over_XTones_ - beta_, 0.0) +
+            (one_over_XTones_ < -beta_).select(one_over_XTones_ + beta_, 0.0);
+        psol_ = temp * inv_alpha_;
+        duality_gap_ = 0.0;
+        return;
+    }
+
+    if (scr_option == 0) {  // alternative safe screening
+                            // and performing sample screening first
+        sifs(true);
+    } else if (scr_option == 1) {  // alternative safe screening
+                                   // and performing feature screening first
+        sifs(false);
+    } else if (scr_option == 2) {  // only sample screening
+        iss();
+    } else if (scr_option == 3) {  // only feature screening
+        ifs();
+    }
+
+    //XTdsol_ =  X*dsol_/n, used in (KKT-1)
+    XTdsol_.setZero();
+    for (int i = 0; i < n_sams_; i++) {
+        if (dsol_[i] > 0.0) {
+            XTdsol_ += dsol_[i] * X_.row(i);
+        }
+    }
+    XTdsol_ *= inv_n_sams_;
+    for (auto &&kk : idx_Fc_vec_) 
+	{
+        psol_[kk] = inv_alpha_ * val_sign(XTdsol_[kk]) *
+            std::max(0.0, std::abs(XTdsol_[kk]) - beta_);
+    }
+    compute_duality_gap(true, false);
+
+    int n_Dc = idx_Dc_vec_.size();
+    if (n_Dc == 0) {
+        std::cout << "  All samples screened. return" << std::endl;
+        return;
+    }
+
+    int ind = 0;
+    const double inv_nalpha_ = inv_n_sams_ * inv_alpha_;
+    double delta_ind = 0.0;
+    double p_theta_ind = 0.0;
+
+    std::default_random_engine rg;
+    std::uniform_int_distribution<> uni_dist(0, n_Dc - 1);
+
+    const auto ins_begin_it = std::begin(idx_Dc_vec_);//
+    auto random_it = std::next(ins_begin_it, uni_dist(rg));
+
+    for (iter_ = 1; iter_ < max_iter_ && duality_gap_ > tol_; ++iter_) 
+	{
+        for (int jj = 0; jj < n_Dc; ++jj) 
+		{
+            random_it = std::next(ins_begin_it, uni_dist(rg));
+            ind = *random_it;
+
+            p_theta_ind = dsol_[ind];
+            delta_ind = (1 - gamma_ * p_theta_ind - (X_.row(ind) * psol_)(0)) /
+                (gamma_ + Xi_norm_sq_[ind] * inv_nalpha_);
+            delta_ind = std::max(-p_theta_ind, std::min(1.0 - p_theta_ind,
+                                                        delta_ind));
+            dsol_[ind] += delta_ind;
+            XTdsol_ +=  (delta_ind * inv_n_sams_) * X_.row(ind);
+
+            for (auto &&kk : idx_Fc_vec_) {
+                psol_[kk] = inv_alpha_ * val_sign(XTdsol_[kk]) *
+                    std::max(0.0, std::abs(XTdsol_[kk]) - beta_);
+            }
+        }
+
+        if (iter_ % chk_fre_ == 0) {
+            compute_duality_gap(true, false);
+            //std::cout<< "    Iter: " << iter_ << " Primal obj: " << get_primal_obj()
+            //         << " Dual obj: " << get_dual_obj()
+            //         << " Duality gap: " << get_duality_gap() << std::endl;
+        }
+    }
+}
+
+
+void s3ifs::clear_idx(void)
+{
+    idx_Fc_vec_.clear();
+    idx_Dc_vec_.clear();
+    std::copy(std::begin(all_ins_index_), std::end(all_ins_index_),
+              std::back_inserter(idx_Dc_vec_));
+    std::copy(std::begin(all_fea_index_), std::end(all_fea_index_),
+              std::back_inserter(idx_Fc_vec_));
+
+    idx_Dc_.clear();
+    idx_Fc_.clear();
+    idx_Dc_.insert(std::begin(all_ins_index_), std::end(all_ins_index_));
+    idx_Fc_.insert(std::begin(all_fea_index_), std::end(all_fea_index_));
+
+    idx_nsv_L_.clear();
+    idx_nsv_R_.clear();
+    idx_F_.clear();
+
+    approx_psol_r_F_sq_ = 0.0;
+    approx_dsol_r_R_sq_ = 0.0;
+    approx_dsol_r_L_sq_ = 0.0;
+    approx_psol_r_sq_ = 0.0;
+    approx_dsol_r_sq_ = 0.0;
+
+    idx_Fc_flag_.setOnes();
+    idx_Dc_flag_.setOnes();
+}
+
+void s3ifs::set_alpha(const double& alpha, const bool& ws) {
+    if (ws) {  // solved problem as warm start and reference solutions
+        ref_alpha_ = alpha_;
+        alpha_ = alpha;
+        inv_alpha_ = 1.0 / alpha_;
+        ref_psol_ = psol_;
+        ref_dsol_ = dsol_;
+    } else {  // no warm start, use naive solution as reference solutions
+        alpha_ = alpha;
+        inv_alpha_ = 1.0 / alpha_;
+        ref_alpha_ = alpha_max_;
+        Eigen::ArrayXd temp =
+            (one_over_XTones_ > beta_).select(one_over_XTones_ - beta_, 0.0) +
+            (one_over_XTones_ < -beta_).select(one_over_XTones_ + beta_, 0.0);
+        ref_psol_ = temp * (1.0 / ref_alpha_);
+        ref_dsol_.resize(n_sams_);
+        ref_dsol_.setOnes();
+
+        // set reference solutions as initial
+        psol_ = ref_psol_;
+        dsol_ = ref_dsol_;
+    }
+
+    dif_alpha_ratio_ = 0.5 * (alpha_ - ref_alpha_) / alpha_;
+    sum_alpha_ratio_ = 0.5 * (alpha_ + ref_alpha_) / alpha_;
+ 
+    duality_gap_ = std::numeric_limits<double>::max();
+    iter_ = 0;
+    fea_scr_time_ = 0.0;
+    sam_scr_time_ = 0.0;
+    scr_time_ = 0.0;
+
+    clear_idx();
+}
+
+void s3ifs::set_beta(const double& beta) {
+    beta_ = beta;
+    if (beta_ >= beta_max_) 
+	{
+        std::cout << "beta >= beta_max: always naive solution" << std::endl;
+    }
+    Eigen::ArrayXd temp =
+        (one_over_XTones_ > beta_).select(one_over_XTones_ - beta_, 0.0) +
+        (one_over_XTones_ < -beta_).select(one_over_XTones_ + beta_, 0.0);
+    alpha_max_ = (1.0/(1.0 - gamma_)) * (X_ * temp.matrix()).maxCoeff();
+    if (alpha_max_ > 0) 
+	{
+        ref_alpha_ = alpha_max_;
+        ref_dsol_.resize(n_sams_);
+        ref_dsol_.setOnes();
+        ref_psol_ = temp * (1.0 / ref_alpha_);
+
+        dif_alpha_ratio_ = 0.5 * (alpha_ - ref_alpha_) / alpha_; //First coeff. of (3)
+        sum_alpha_ratio_ = 0.5 * (alpha_ + ref_alpha_) / alpha_; //second coeff. of (3)
+    } 
+	else 
+	{
+        ref_alpha_ = 0;
+        ref_dsol_ = Eigen::VectorXd::Ones(n_sams_);
+        ref_psol_ = Eigen::VectorXd::Zero(n_feas_);
+
+        dif_alpha_ratio_ = 0.5;
+        sum_alpha_ratio_ = 0.5;
+    }
+    
+    duality_gap_ = std::numeric_limits<double>::max();
+    iter_ = 0;
+    fea_scr_time_ = 0.0;
+    sam_scr_time_ = 0.0;
+    scr_time_ = 0.0;
+
+    clear_idx();
+}
+
+void s3ifs::sample_screening(void) 
+{
+    auto start_time = sys_clk::now();
+
+    const double L_coeff = 0.5 * ((2 * gamma_ - 1) * alpha_ + ref_alpha_) *
+        inv_alpha_ * inv_gamma_; //used in the second item of (6)
+
+    double psol_radius =
+        std::sqrt(std::max(0.0, dif_alpha_ratio_ * dif_alpha_ratio_ *
+                           approx_psol_r_sq_ - sum_alpha_ratio_ *
+                           sum_alpha_ratio_ * approx_psol_r_F_sq_));
+
+    double temp, xiw_comp_lb, xiw_comp_ub, Xi_Fc_sq, Xi_Fc_approx_psol_Fc;
+    std::vector<int> new_nsv_L, new_nsv_R;
+
+    for (auto &&i : idx_Dc_) 
+	{
+        Xi_Fc_sq = 0.0;
+
+        for (srm_iit it(X_, i); it; ++it) 
+		{
+            if (idx_Fc_flag_[it.index()])
+                Xi_Fc_sq += it.value() * it.value(); //Used in (10)
+        }
+
+        Xi_Fc_approx_psol_Fc = sum_alpha_ratio_ * (X_.row(i) * ref_psol_)(0);
+        temp = psol_radius * std::sqrt(Xi_Fc_sq); //See Lemma 5
+        xiw_comp_ub = 1 - Xi_Fc_approx_psol_Fc + temp;
+        xiw_comp_lb = 1 - Xi_Fc_approx_psol_Fc - temp;
+ 
+        if (xiw_comp_ub <  - 1e-9) 
+		{
+            new_nsv_R.push_back(i);
+        }
+		else if (xiw_comp_lb > gamma_ + 1e-9) 
+		{
+            new_nsv_L.push_back(i);
+        }
+    }
+
+    for (auto &&i : new_nsv_R) 
+	{
+        dsol_[i] = 0.0;
+        idx_Dc_flag_[i] = 0.0;
+        idx_nsv_R_.insert(i);
+        idx_Dc_.erase(i);
+        approx_dsol_c_[i] = 0.0;
+        temp = dif_alpha_ratio_ * inv_gamma_ + sum_alpha_ratio_ * ref_dsol_[i]; //Third item of (6)
+        approx_dsol_r_R_sq_ += temp * temp; //Third item of (6)
+    }
+    for (auto &&i : new_nsv_L) 
+	{
+        dsol_[i] = 1.0;
+        idx_Dc_flag_[i] = 0.0;
+        idx_nsv_L_.insert(i);
+        idx_Dc_.erase(i);
+        approx_dsol_c_[i] = 1.0;
+        temp = L_coeff - sum_alpha_ratio_ * ref_dsol_[i];//second item of (6)
+        approx_dsol_r_L_sq_ += temp * temp;//second item of (6)
+    }
+
+    auto end_time = sys_clk::now();
+    sam_scr_time_ += static_cast<double>(
+        std::chrono::duration_cast<mil_sec>(end_time - start_time).count());
+}
+
+void s3ifs::feature_screening(void) 
+	{
+    auto start_time = sys_clk::now();
+
+    double dsol_radius =
+        std::sqrt(std::max(0.0, dif_alpha_ratio_ * dif_alpha_ratio_ *
+                           approx_dsol_r_sq_ - approx_dsol_r_L_sq_
+                           - approx_dsol_r_R_sq_)); //formular (6)
+
+    double Xj_Dc_sq, temp, XjTdsol_ub;
+    std::vector<int> new_iaf;
+    double n_beta = n_sams_ * beta_;
+    for (auto &&j : idx_Fc_) 
+	{
+        Xj_Dc_sq = 0.0;
+
+        for (scm_iit it(X_CM_, j); it; ++it) 	
+		{
+            if (idx_Dc_flag_[it.index()])
+			{
+                Xj_Dc_sq += it.value() * it.value();
+            }
+        }
+
+        temp = (approx_dsol_c_.transpose() * X_CM_.col(j))(0);//the first item of Lemma 4
+        XjTdsol_ub = std::abs(temp) + dsol_radius * std::sqrt(Xj_Dc_sq); //see Lemma 4
+		
+		// This is the (IFS) in Theorem 4
+        if (XjTdsol_ub <= n_beta - 1e-9) {
+            new_iaf.push_back(j);
+        }
+    }
+
+    for (auto && j : new_iaf) 
+	{
+        idx_Fc_flag_[j] = 0.0;
+        idx_Fc_.erase(j);
+        idx_F_.insert(j);
+        psol_[j] = 0.0;
+        approx_psol_r_F_sq_ += ref_psol_[j] * ref_psol_[j]; //second squre of (3)
+        ref_psol_[j] = 0.0;
+    }
+
+    auto end_time = sys_clk::now();
+    fea_scr_time_ += static_cast<double>(
+        std::chrono::duration_cast<mil_sec>(end_time - start_time).count());
+}
+
+
+void s3ifs::sifs(const bool& sample_scr_first) 
+{
+    auto start_time = sys_clk::now();
+    approx_dsol_c_ =
+        (dif_alpha_ratio_ * inv_gamma_) + sum_alpha_ratio_ * ref_dsol_.array();//formular (5)
+
+    approx_psol_r_sq_ = ref_psol_.squaredNorm(); //first squre of (3)
+    approx_dsol_r_sq_ = (ref_dsol_.array() - inv_gamma_).square().sum();//first square of (6)
+ 
+    //initialization
+    int n_Dc = n_sams_;
+    int n_Fc = n_feas_;
+    int n_Dc_pre = n_Dc;
+    int n_Fc_pre = n_Fc;
+    int scr_iter = 0;
+    
+    while (true)
+	{
+        scr_iter++;
+        n_Dc_pre = n_Dc;
+        n_Fc_pre = n_Fc;
+        if (sample_scr_first) 
+		{
+            sample_screening();
+            feature_screening();
+        } 
+		else 
+		{
+            feature_screening();
+            sample_screening();
+        }
+        n_Dc = idx_Dc_.size();
+        n_Fc = idx_Fc_.size();
+
+        if ((n_Dc == n_Dc_pre) && (n_Fc == n_Fc_pre)) 
+		{
+            break;
+        }
+        if (scr_max_iter_ > 0 && scr_iter >= scr_max_iter_) 
+		{
+            break;
+        }
+    }
+
+    idx_Dc_vec_.clear();
+    idx_Fc_vec_.clear();
+
+    for (auto &&j : idx_Dc_) 
+	{
+        idx_Dc_vec_.push_back(j);
+    }
+    for (auto &&j : idx_Fc_) 
+	{
+        idx_Fc_vec_.push_back(j);
+    }
+    auto end_time = sys_clk::now();
+    scr_time_ = static_cast<double>(
+        std::chrono::duration_cast<mil_sec>(end_time - start_time).count());
+}
+
+void s3ifs::ifs(void) 
+{
+    auto start_time = sys_clk::now();
+    approx_dsol_c_ =
+        (dif_alpha_ratio_ * inv_gamma_) + sum_alpha_ratio_ * ref_dsol_.array();
+    approx_psol_r_sq_ = ref_psol_.squaredNorm();
+    approx_dsol_r_sq_ = (ref_dsol_.array() - inv_gamma_).square().sum();
+ 
+    feature_screening();
+
+    idx_Fc_vec_.clear();
+    for (auto &&j : idx_Fc_) {
+        idx_Fc_vec_.push_back(j);
+    }
+    auto end_time = sys_clk::now();
+    scr_time_ = static_cast<double>(
+        std::chrono::duration_cast<mil_sec>(end_time - start_time).count());
+}
+
+void s3ifs::iss(void) 
+{
+    auto start_time = sys_clk::now();
+    approx_dsol_c_ = (dif_alpha_ratio_ * inv_gamma_) + sum_alpha_ratio_ * ref_dsol_.array();
+    approx_psol_r_sq_ = ref_psol_.squaredNorm();
+    approx_dsol_r_sq_ = (ref_dsol_.array() - inv_gamma_).square().sum();
+ 
+    sample_screening();
+
+    idx_Dc_vec_.clear();
+    for (auto &&j : idx_Dc_) {
+        idx_Dc_vec_.push_back(j);
+    }
+    auto end_time = sys_clk::now();
+    scr_time_ = static_cast<double>(
+        std::chrono::duration_cast<mil_sec>(end_time - start_time).count());
+}
+
+void s3ifs::parse_command_line() 
+{
     // default parameters
     rbu = 1.0;
     rbl = 0.05;
--- struck/src/s3ifs.h
+++ struck/src/s3ifs.h
@@ -8,14 +8,85 @@
 #include <cstdlib>
 #include <cmath>
 
+#include <Eigen/Core>
+#include <Eigen/SparseCore>
+#include <sdm/lib/io.hpp>
+#include <random>
+#include <numeric>
+#include <vector>
+#include <iterator>
+#include <chrono>
+#include <unordered_set>
+
+
 extern std::ofstream trainingLogFile;
+
 using SpMatRd = Eigen::SparseMatrix<double, Eigen::RowMajor>;
+using SpMatCd = Eigen::SparseMatrix<double, Eigen::ColMajor>;
+using scm_iit = Eigen::SparseMatrix<double, Eigen::ColMajor>::InnerIterator;
+using srm_iit = Eigen::SparseMatrix<double, Eigen::RowMajor>::InnerIterator;
+
+using sec = std::chrono::seconds;
+using mil_sec = std::chrono::milliseconds;
+using sys_clk = std::chrono::system_clock;
+
+using u_set = std::unordered_set<int>;
 
 class s3ifs
 {
 public:
-	s3ifs(SpMatRd X, SpMatRd D);
+	s3ifs(SpMatRd X_, SpMatRd D_);
 	~s3ifs();
+
+    int get_n_sams(void) const;
+    int get_n_feas(void) const;
+
+    double get_primal_obj(void) const;
+    double get_dual_obj(void) const; 
+    double get_duality_gap(void) const; 
+
+    Eigen::VectorXd get_dual_sol(void) const;
+    Eigen::VectorXd get_primal_sol(void) const;
+    
+    void set_alpha(const double& alpha, const bool& ws = true);
+    void set_beta(const double& beta);
+    void set_stop_tol(const double& tol);
+    
+    void compute_primal_obj(const bool& flag_comp_loss); // compute primal objective
+    void compute_dual_obj(const bool& flag_comp_XTdsol); // compute dual objective
+    void compute_duality_gap(const bool& flag_comp_loss,
+                             const bool& flag_comp_XTdsol); // compute duality gap
+ 
+    void update_psol(const bool& flag_comp_XTdsol);
+
+    void train(void);
+    void train_sifs(const int& scr_option = 0);
+
+    void sample_screening(void);
+    void feature_screening(void);
+ 
+    void sifs(const bool& sample_scr_first = true);
+    void ifs(void);
+    void iss(void);
+
+    void clear_idx(void);
+
+    int get_n_L(void) const;
+    int get_n_R(void) const;
+    int get_n_F(void) const;
+    int get_iter(void) const;
+
+    double get_alpha_max(void) const;
+    double get_beta_max(void) const;
+
+    SpMatRd X_;  // \bar{X}, each row contains one sample
+    SpMatRd D_;  // \bar{X}, each row contains one sample
+    SpMatCd X_CM_;  // column major representation of \bar{X}
+    //Eigen::ArrayXd y_;  // training labels
+    
+    double fea_scr_time_;
+    double sam_scr_time_;
+    double scr_time_;
 
 private:
 	int log_yn = 1;
@@ -27,6 +98,71 @@ private:
 	int task;
 
 	void parse_command_line();
+
+    int n_sams_;
+    int n_feas_;
+
+    double alpha_;
+    double beta_;
+    double gamma_;
+    double tol_;
+    int max_iter_;
+    int chk_fre_;
+    int iter_;
+ 
+    double alpha_max_;
+    double beta_max_;
+    int scr_max_iter_;
+
+    double pobj_;
+    double dobj_;
+    double duality_gap_;
+    double loss_;
+
+    double inv_n_sams_;
+    double inv_alpha_;
+    double inv_gamma_;
+
+    Eigen::ArrayXd one_over_XTones_;  // \bar{X}^T * ones / n
+    Eigen::VectorXd psol_;
+    Eigen::VectorXd dsol_;
+    Eigen::VectorXd XTdsol_; // \bar{X}^T * theta / n
+    Eigen::ArrayXd Xw_comp_; // 1 - \bar{X} * w
+    
+    Eigen::ArrayXd Xi_norm_;
+    Eigen::ArrayXd Xi_norm_sq_;
+    Eigen::ArrayXd Xj_norm_;
+    Eigen::ArrayXd Xj_norm_sq_;
+    
+    std::vector<int> all_ins_index_;
+    std::vector<int> all_fea_index_;
+    u_set idx_nsv_L_;
+    u_set idx_nsv_R_;
+    u_set idx_Dc_;
+    u_set idx_F_;
+    u_set idx_Fc_;
+    std::vector<int> idx_Dc_vec_;
+    std::vector<int> idx_Fc_vec_;
+    Eigen::VectorXd idx_Dc_flag_;
+    Eigen::VectorXd idx_Fc_flag_;
+    
+    double ref_alpha_; // alpha_0
+    double dif_alpha_ratio_; // (alpha - alpha_0) / alpha
+    double sum_alpha_ratio_; // (alpha + alpha_0) / alpha
+    Eigen::VectorXd ref_psol_;  // reference primal solution w_0,
+                                //also center of primal optimum estimation
+    Eigen::VectorXd ref_dsol_;
+    Eigen::VectorXd approx_dsol_c_;  // center of dual optimum estimation
+    double approx_dsol_r_sq_;  // square of radius of dual optimum estimation
+    double approx_dsol_r_L_sq_;
+    double approx_dsol_r_R_sq_;
+    double approx_psol_r_sq_;  // |w_0|^2
+    double approx_psol_r_F_sq_;  // |w_0(F)|^2
+
 };
+
+template <typename _Tp> inline _Tp val_sign(_Tp val) {
+    return 1.0 - (val <= 0.0) - (val < 0.0);
+}
 
 #endif
